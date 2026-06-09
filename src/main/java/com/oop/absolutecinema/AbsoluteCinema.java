0afb5d8720acf42783baed0e8d4f6f87f3652d1b
K <thegr8kii@gmail.com>
2026-06-09 18:04:02 +0700

    Setup Springboot

---
+++ src/main/java/com/oop/absolutecinema/AbsoluteCinema.java
@@ -0,0 +1,13 @@
+package com.oop.absolutecinema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbsoluteCinema {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AbsoluteCinema.class, args);
+    }
+
+}
